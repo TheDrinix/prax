fc18a3489dbe918f5e85b07451027a24e56eedb0
TheDrinix <thedrinix@gmail.com>
2022-02-10 15:40:15 +0100

    Change background

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ store.commit('getTodayDate');
 <template>
     <Header />
     <router-view v-slot="{ Component }">
-        <transition name="fade" mode="in-out">
+        <transition name="moveUp">
             <component :is="Component" />
         </transition>
     </router-view>
@@ -22,23 +22,52 @@ body {
     width: 100%;
     min-height: 100vh;
     background: #fff;
-    background-image: url('/src/assets/5570834.jpg');
+    background-image: url('/src/assets/low-poly-grid-haikei.svg');
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
+    background-attachment: fixed;
 }
 
 .is-today {
     border: solid 2px lime !important;
 }
 
-.fade-enter-active,
+/* .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 0.4s;
+    transition: opacity 0.4s, transform 0.4s;
 }
 
 .fade-enter,
 .fade-leave-to {
     opacity: 0;
+    transform: translateX(-30%);
+} */
+
+.moveUp-enter-active {
+    animation: fadeIn 1s ease-in-out;
+}
+@keyframes fadeIn {
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 0.5;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+
+.moveUp-leave-active {
+    animation: moveUp 0.3s ease-in;
+}
+@keyframes moveUp {
+    0% {
+        transform: translateY(0);
+    }
+    100% {
+        transform: translateY(-400px);
+    }
 }
 </style>
